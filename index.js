747ce770f22195d989d670fb03b59d2a43db0342
unknown <aliiobeid01@gmail.com>
2022-09-16 23:25:43 +0300

    create function to flip the card

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ let flipped = false;
 let gamelocked = false;
 let card1, card2;
 let match=0;
+
+reset.addEventListener('click', restart);
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+function flipCard() {
+  if (gamelocked ) return;
+  if (this === card1) return;
+
+  this.classList.add('flip');
+
+  if (!flipped) {
+    flipped = true;
+    card1 = this;
+
+    return;
+  }
+
+
+  card2 = this;
+  check_macth_card();
+}
